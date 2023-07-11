616883909c1e04da6d0eab5a5e1d701e18ea9c1b
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-11 19:16:11 +0430

    Create add_book_spec.rb

---
+++ spec/add_book_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../book_utilities/add_book'
+require_relative '../label_utilities/add_label'
+require_relative '../book'
+require 'pry'
+describe AddBook do
+  let(:publisher) { 'Elham' }
+  let(:cover_state) { 'bad' }
+  let(:publish_date) { '2019-01-01' }
+  let(:book) { Book.new(publish_date, publisher, cover_state) }
+  let(:label) { Label.new('title', 'color') }
+  let(:add_book) { AddBook.new }
+  let(:add_label) { AddLabel.new }
+
+  it '.#add_book should add a book' do
+    expect { add_book.add_books(book, label) }.to change { add_book.book.count }.by(1)
+  end
+end
