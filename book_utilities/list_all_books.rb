4627060cb9a13d3d0afc5859509e4ee4a66e19bf
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-11 19:12:57 +0430

    Create list_all_books.rb

---
+++ book_utilities/list_all_books.rb
@@ -0,0 +1,20 @@
+require 'pry'
+class ListBook
+  attr_accessor :books
+
+  def initialize
+    @books = []
+  end
+
+  def list_all_books
+    if @books == []
+      puts 'The books is empty.'
+    else
+      @books.each do |book|
+        puts "Publish date: #{book.publish_date}, " \
+             "Publisher: #{book.publisher}, " \
+             "Cover state: #{book.cover_state}"
+      end
+    end
+  end
+end
