ace262b09b4aa1fa602c2bad52cba37578946032
Chritelle-12 <mariechristellenirere@gmail.com>
2023-07-12 06:18:14 +0200

    Create Author class

---
+++ author_class.rb
@@ -0,0 +1,19 @@
+require_relative 'item_class'
+
+class Author < Item
+  attr_accessor :first_name, :last_name
+  attr_reader :id, :items
+
+  def initialize(first_name, last_name)
+    super()
+    @id = rand(1..1000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.author = self
+  end
+end
