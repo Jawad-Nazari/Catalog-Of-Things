38607173127c85595d884ee67e5ae22ba095a75f
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-12 17:07:16 +0430

    Update list_all_labels.rb

--- label_utilities/list_all_labels.rb
+++ label_utilities/list_all_labels.rb
@@ -1,15 +1,10 @@
 class ListLabel
-  attr_accessor :labels
-
-  def initialize
-    @labels = []
-  end
-
-  def list_all_labels
-    if @labels == []
+  def list_all_labels(data)
+    if [[], nil].include?(data)
       puts 'The labels is empty.'
     else
-      @labels.each do |label|
+      data.each do |label|
+        label = label = Label.new(label[:title], label[:color]) if label.is_a?(Hash)
         puts "Title: #{label.title}, Color: #{label.color}"
       end
     end
