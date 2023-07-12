7270b1b271a1247fe1c8ae4afed145d143b82dd2
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-12 18:19:22 +0430

    Update add_label.rb
    Add comments

--- label_utilities/add_label.rb
+++ label_utilities/add_label.rb
@@ -1,10 +1,12 @@
 class AddLabel
   attr_accessor :label
 
+  # This method is used to initialize the label array.
   def initialize
     @label = []
   end
 
+  # This method is used to add a label to the label array.
   def add_label(label)
     @label << label
   end
